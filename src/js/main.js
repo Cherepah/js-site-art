839c352a4738bdfdad6d8e03ef716bcea95d3eec
Cherepah <dotacontra@mailru>
2021-03-29 16:32:54 +0300

    forms

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
 import modal from "./modules/modal";
 import sliders from "./modules/slider";
+import forms from "./modules/form";
 
 window.addEventListener('DOMContentLoaded', () => {
     modal();
     sliders('.feedback-slider-item', 'h', '.main-prev-btn', '.main-next-btn');
     sliders('.main-slider-item', 'vertical');
+    forms();
 });
