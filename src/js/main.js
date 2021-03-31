bade350aff7ce6889de5a92552bf5be25e947f6f
Cherepah <dotacontra@mailru>
2021-03-31 14:32:46 +0300

    pictureSize

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,7 @@ import checkTextInput from "./modules/checkTextInput";
 import showMoreStyles from "./modules/showMore";
 import calc from "./modules/calc";
 import filter from "./modules/filter";
+import pictureSize from "./modules/pictureSize";
 
 window.addEventListener('DOMContentLoaded', () => {
     modal();
@@ -18,4 +19,5 @@ window.addEventListener('DOMContentLoaded', () => {
     showMoreStyles('.button-styles', '#styles .row');
     calc('#size', '#material', '#options', '.promocode', '.calc-price');
     filter();
+    pictureSize('.sizes-block');
 });
