45db43562c5cf1a602960f3386b7bd4dfc63447d
Cherepah <dotacontra@mailru>
2021-03-31 11:10:47 +0300

    db.json showMore

--- src/js/modules/form.js
+++ src/js/modules/form.js
@@ -1,3 +1,5 @@
+import {postData} from '../services/requests';
+
 const forms = (state) => {
     const form = document.querySelectorAll('form'),
           inputs = document.querySelectorAll('input'),
@@ -17,15 +19,6 @@ const forms = (state) => {
     const path = {
         designer: 'assets/server.php',
         question: 'assets/question.php'
-    };
-
-    const postData = async (url, data) => {
-        let res = await fetch(url, {
-            method: "POST",
-            body: data
-        });
-
-        return await res.text();
     };
 
     const clearInputs = () => {
